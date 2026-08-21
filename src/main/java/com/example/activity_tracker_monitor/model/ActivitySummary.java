253dfe976943ee17aea7533dd1001a7e9ffdad6a
Shubham Srivastava <shubham.srivastava1207@gmail.com>
2026-08-21 12:18:12 +0530

    update in the project

---
+++ src/main/java/com/example/activity_tracker_monitor/model/ActivitySummary.java
@@ -0,0 +1,31 @@
+package com.example.activity_tracker_monitor.model;
+
+
+import jakarta.persistence.*;
+import lombok.Data;
+import java.time.LocalDate;
+
+@Entity
+@Table(
+        name = "activity_summary",
+        uniqueConstraints = @UniqueConstraint(columnNames = {"employeeId", "date"})
+)
+@Data
+public class ActivitySummary {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private Long employeeId;
+
+    @Column(nullable = false)
+    private LocalDate date;
+
+    @Column(nullable = false)
+    private long totalActiveSeconds;
+
+    @Column(nullable = false)
+    private long totalIdleSeconds;
+}
