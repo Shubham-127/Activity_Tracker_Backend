b71d5cfaa5fa22f02d2570012a31c12625c28cdf
Shubham Srivastava <shubham.srivastava1207@gmail.com>
2026-09-08 12:33:25 +0530

    update in the project

---
+++ src/main/java/com/example/activity_tracker_monitor/security/CorsConfig.java
@@ -0,0 +1,24 @@
+package com.example.activity_tracker_monitor.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.service.invoker.UrlArgumentResolver;
+
+import java.util.List;
+
+@Configuration
+public class CorsConfig {
+    @Bean
+    public UrlBasedCorsConfigurationSource corsConfigurationSource(){
+        CorsConfiguration config = new CorsConfiguration();
+        config.setAllowedOrigins(List.of("*"));
+        config.setAllowedMethods(List.of("GET","POST","PUT","DELETE","OPTIONS"));
+        config.setAllowedHeaders(List.of("*"));
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", config);
+        return source;
+    }
+}
