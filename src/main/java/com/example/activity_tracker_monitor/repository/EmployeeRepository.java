eb636a97573c3a21cbb63539dee382b148d0bc35
Shubham Srivastava <shubham.srivastava1207@gmail.com>
2026-09-09 12:02:19 +0530

    update in the project

--- src/main/java/com/example/activity_tracker_monitor/repository/EmployeeRepository.java
+++ src/main/java/com/example/activity_tracker_monitor/repository/EmployeeRepository.java
@@ -3,8 +3,10 @@ package com.example.activity_tracker_monitor.repository;
 import com.example.activity_tracker_monitor.model.Employee;
 import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.List;
+import java.util.Optional;
 
 public interface EmployeeRepository extends JpaRepository<Employee, Long> {
     List<Employee> findByManagerId(Long managerId);
     boolean existsByIdAndManagerId(Long employeeId, Long managerId);
+    Optional<Employee> findByEmail(String email);
 }
